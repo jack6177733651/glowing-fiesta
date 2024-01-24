ee12c0a0436b4c2975c46739b47dcffe5868ec00
jack6177733651 <jackshi3@gmail.com>
2024-01-24 11:27:14 -0500

    adding question controller

--- src/main/java/com/example/demo/QuestionController.java
+++ src/main/java/com/example/demo/QuestionController.java
@@ -1,4 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class QuestionController {
+
+    @GetMapping
+    public String getQuestion() {
+        return "question";
+    }
 }
